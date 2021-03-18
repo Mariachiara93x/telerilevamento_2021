b45e1efee754ec17e823c3ec5887b02695dea203
Mariachiara93x <80513539+Mariachiara93x@users.noreply.github.com>
2021-03-18 13:10:13 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -10,3 +10,17 @@ p224r63_2011<-brick("p224r63_2011_masked.grd")
 p224r63_2011
 
 plot(p224r63_2011)
+
+
+# colour change
+cl <- colorRampPalette(c("black","grey","light grey")) (100)
+
+plot(p224r63_2011, col=cl)
+
+
+# colour change -> new
+cl <- colorRampPalette(c("pink", "light blue", "dark green", "purple", "orange", "dark red")) (200)
+plot(p224r63_2011, col=cl)
+
+
+
