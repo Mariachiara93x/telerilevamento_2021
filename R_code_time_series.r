55f838cb572c7afe183b73c9b3a11b7b9477b5ac
Mariachiara93x <80513539+Mariachiara93x@users.noreply.github.com>
2021-04-09 12:41:43 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -64,3 +64,47 @@ plot(TGr)
 plotRGB(TGr,1,2,3,stretch="Lin")
 plotRGB(TGr,2,3,4,stretch="Lin")
 plotRGB(TGr,4,3,2,stretch="Lin")
+
+#installo pacchetto rasterVis
+install.packages(rasterVis)
+
+#day 3
+setwd("C:/lab/Greenland/")
+library(raster)
+library(rasterVis)
+
+rlist <- list.files(pattern="lst")
+rlist
+
+import <- lapply(rlist,raster)
+import
+TGr <- stack(import)
+TGr
+levelplot(TGr)
+
+
+levelplot(TGr$lst_2000)
+
+cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
+levelplot(TGr, col.regions=cl)
+
+levelplot(TGr,col.regions=cl, names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+    
+# Melt
+meltlist <- list.files(pattern="melt")
+melt_import <- lapply(meltlist,raster)
+melt <- stack(melt_import)
+melt
+
+levelplot(melt)
+melt_amount <- melt$X2007annual_melt - melt$X1979annual_melt
+clb <- colorRampPalette(c("blue","white","red"))(100)
+plot(melt_amount, col=clb) 
+levelplot(melt_amount, col.regions=clb)
+
+#installo pacchetto knitr
+install.packages("knitr")
+
+setwd("C:/lab/Greenland/")
+library(raster)
+library(knitr)
