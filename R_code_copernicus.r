93e8c938c679266a0f0126160475cad04038635e
Mariachiara93x <80513539+Mariachiara93x@users.noreply.github.com>
2021-04-17 11:41:50 +0200

    Create R_code_copernicus.r

---
+++ R_code_copernicus.r
@@ -0,0 +1,9 @@
+#R_code_copernicus.r
+#Visualizing Copernicus data
+#utilizzo il programma Copernicus per visualizzare dati relativi a variabili bio-geofisiche della superficie terrestre
+
+#installo il pacchetto ncdf4 per leggere il formato netCDF
+install.packages("ncdf4")
+library(raster)
+library(ncdf4)
+
