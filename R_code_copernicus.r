45f84936fed277e8aa3198d9e3dc847c2b3fbe11
Mariachiara93x <80513539+Mariachiara93x@users.noreply.github.com>
2021-04-17 12:47:30 +0200

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -7,3 +7,17 @@ install.packages("ncdf4")
 library(raster)
 library(ncdf4)
 
+setwd("C:/lab/") 
+
+criosfera <- raster ("c_gls_SCE500_202104150000_CEURO_MODIS_V1.0.1.nc")
+
+cl <- colorRampPalette(c("light blue", "red", "green", "orange")) (200)
+plot(criosfera, col=cl)
+
+#Resampling
+#utilizzo la funzione aggregate per portare l'immagine ad un numero inferiore di pixel
+#in questo modo l'immagine sarà meno pesante
+#utilizzo il fattore "fact=10" per diminuire linearmente di 10 volte
+#in questo modo, ogni 10 pixel avrò 1 solo pixel
+criosferares <- aggregate(criosfera, fact=10)
+plot(criosferares, col=cl)
