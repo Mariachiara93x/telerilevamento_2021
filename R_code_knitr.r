9ea5cd57ccc6f486fba9e2061da794b2a3f0c5cb
Mariachiara93x <80513539+Mariachiara93x@users.noreply.github.com>
2021-04-19 15:00:37 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -1,3 +1,9 @@
 #R_code_knitr.r
+#utilizzo il pacchetto knitr per usufruire di un codice esterno
+#tramite il codice esterno, importato in R, viene generato un report
+#il report sarà poi salvato nella stessa cartella in cui è presente il codice esterno
 
 setwd("C:/lab/")
+
+library(knitr)
+
