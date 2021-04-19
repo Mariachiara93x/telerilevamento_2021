cfbc2024cfec2e53017e31b86bc14d9741968072
Mariachiara93x <80513539+Mariachiara93x@users.noreply.github.com>
2021-04-19 16:24:22 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -7,3 +7,12 @@ setwd("C:/lab/")
 
 library(knitr)
 
+#creo un file di testo con il codice creato all'interno di R_code_time series
+#rinomino il file con "R_code_greenland.r"
+#inserisco il file di testo all'interno della cartella lab
+
+#utilizzo la funzione stitch per creare il report
+#inserisco come argomento per stitch il nome del file "R_code_greenland.txt"
+#con tutta questa operazione salverò nella cartella lab il report generato
+stitch("R_code_greenland.r.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+
