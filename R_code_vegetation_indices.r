6a2c5095d6a63fba0b9410255d5f9184cc0d69d1
Mariachiara93x <80513539+Mariachiara93x@users.noreply.github.com>
2021-04-30 16:49:25 +0200

    Update R_code_vegetation_indices.r

--- R_code_vegetation_indices.r
+++ R_code_vegetation_indices.r
@@ -6,13 +6,17 @@ setwd("C:/lab/")
 defor1 <- brick("defor1.jpg")
 defor2 <- brick("defor2.jpg")
 
+#plotto con RGB un par di entrambe le immagini
+#osservo la variazione di vegetazione nel tempo
+#b1=NIR, b2=rosso, b3=verde
 par(mfrow=c(2,1))
 plotRGB(defor1, r=1, g=2, b=3, stretch="lin")
 plotRGB(defor2, r=1, g=2, b=3, stretch="lin")
 
 #visualizzo informazioni su immagine defor1
 defor1
-#difference vegetetion index
+
+#DIffERENCE VEGETATION INDEX
 #osservo in che stato di salute è la vegetazione
 #noto che la vegetazione (foresta amazzonica) è molto verde
 dvi1 <- defor1$defor1.1 - defor1$defor1.2
