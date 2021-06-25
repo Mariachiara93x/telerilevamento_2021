61df0e84ef53e77fd7411af1033b3de5a8a06288
Mariachiara93x <80513539+Mariachiara93x@users.noreply.github.com>
2021-06-25 11:02:01 +0200

    Update R_code_no2.r

--- R_code_no2.r
+++ R_code_no2.r
@@ -44,3 +44,37 @@ par(mfrow=c(3,1))
 plot(EN01, col=cl, main="NO2 in January")
 plot(EN013, col=cl, main="NO2 in March")
 plot(ENdif2, col=cl, main="Difference (January - March)")
+
+#Importo tutto il set di immagini
+#per selezionare una lista di files utilizzo la funzione list.files, scegliendo un pattern comune a tutti ("EN")
+#per importare il set di dati utilizzo la funzione lapply, applicando la funz raster a tutti gli elementi
+rlist <- list.files(pattern="EN")
+import <- lapply(rlist,raster)
+import
+
+#la funzione stack permette di raggruppare un numero di file raster tutti assieme in un unico blocco
+#plotto lo stack creato nella colorramppalette utilizzata anche prima
+EN <- stack(import)
+plot(EN, col=cl)
+
+# Replicare il plot dell'immagine 1 e 13 usando lo stack
+# ossia, pesco le immagini 1 e 13 direttamente dallo stack e poi lo plotto con par
+par(mfrow=c(2,1))
+plot(EN$EN_0001, col=cl)
+plot(EN$EN_0013, col=cl)
+
+# Faccio una PCA relativamente alle 13 immagini
+#per fare la PCA serve il pacchetto RStoolbox
+library(RStoolbox)
+ENpca<-rasterPCA(EN)
+summary(ENpca$model)
+plotRGB(ENpca$map, r=1, g=2, b=3, stretch="lin")
+#gran parte dell'informazione è nella componente red: in rosso è ciò che si è mantenuto circa stabile all'interno del set di dati
+
+#Calcola la variabilità (local standard deviation) della prima PCA
+PC1sd <- focal (ENpca$map$PC1, w=matrix(1/9, nrow=3, ncol=3), fun=sd)
+plot(PC1sd, col=cl)
+#osservo che la stadard deviation aumenta dove c'è la linea di separazione tra un Paese e l'altro
+
+
+
