801b11af1c7b3d4f67ea5bfe835b3408408fcfc5
Mariachiara93x <80513539+Mariachiara93x@users.noreply.github.com>
2021-06-25 15:34:01 +0200

    Create R_code_spectral_signatures.r

---
+++ R_code_spectral_signatures.r
@@ -0,0 +1,6 @@
+#R_code_spectral_signatures.r
+# la firma spettrale è una sorta di impronta digitale della relativa specie di un organismo, habitat, roccia, minerale, ecc
+# è specifica per ogni combinazione di riflessi e assorbimenti delle radiazioni elettromagnetiche (EM) a diverse lunghezze d'onda.
+
+library (raster)
+setwd("C:/lab/")
