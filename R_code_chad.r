8b145635800075f694f6fa95cd43f18cdb13c286
Mariachiara93x <80513539+Mariachiara93x@users.noreply.github.com>
2021-07-04 11:41:38 +0200

    Update R_code_chad.r

--- R_code_chad.r
+++ R_code_chad.r
@@ -18,8 +18,8 @@ library(raster)
 setwd("C:/lab/CH")
 #importo le tre immagini del Lago Chad (in falso colore) telerilevate rispettivamente da Landsat 1 (1973), Landsat 5 (1987) e Landsat 8 in 2017
 #le immagini sono state scaricate da Earth Observatory (https://earthobservatory.nasa.gov/features/LakeChad)
-#utilizzo la funzione brick per importare tutte le bande disponibili (tre per ciascuna immagine in questo caso)
 
+#utilizzo la funzione brick per importare tutte le bande disponibili (tre per ciascuna immagine in questo caso)
 chad1 <- brick("chad1973.jpg")
 chad1
 plot(chad1)
@@ -41,9 +41,8 @@ plotRGB(chad1, r=1, g=2, b=3, stretch="lin")
 plotRGB(chad2, r=1, g=2, b=3, stretch="lin")
 plotRGB(chad3, r=1, g=2, b=3, stretch="lin")
 
-
-
 #NDVI
+#calcolo NDVI per le tre immagini per ottenere informazioni sullo stato della vegetazione
 #(NIR-RED) / (NIR+RED)
 cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100)
 ndvi1 <- (chad1$chad1973.1 - chad1$chad1973.2) / (chad1$chad1973.1 + chad1$chad1973.2)
@@ -53,63 +52,54 @@ plot(ndvi2, col=cl, main="NDVI in 1987")
 ndvi3 <- ( chad3$chad2017.1 - chad3$chad2017.2) / ( chad3$chad2017.1 + chad3$chad2017.2)
 plot(ndvi3, col=cl,main="NDVI in 2017")
 
+#faccio un par delle tre immagini per confrontare i valori NDVI
 par(mfrow=c(2,2))
 plot(ndvi1, col=cl, main="NDVI in 1973") 
 plot(ndvi2, col=cl, main="NDVI in 1987")
 plot(ndvi3, col=cl, main="NDVI in 2017")
-#per immagine 1973 osservo: valori NDVI da -1 a -0.4 (presenza di acqua nel lago); ai bordi nella zona SE valori vicini a 0.5
-#per immagine 1987 osservo: valori NDVI circa 0.4 (rimanenza lago a SE); intorno al lago a SE valore 1 (fitta vegetazione)
-#per immagine 2017 osservo: valori NDVI vicini a -1 (rimanenza lago a SE); il resto del lago riporta valori tra 0.5 e 1 (aumenta superfice vegetativa)
+#per immagine 1973 osservo: valori NDVI da -1 a -0.6 (presenza di acqua nel lago - blu scuro ); ai bordi nella zona SE valori vicini a 0.5 (vegetazione), giallo= mix acqua/sedimento
+#per immagine 1987 osservo: valori NDVI circa -0.3 -0.6 (rimanenza lago a SE, blu-giallo= acqua/sedimento/(vegetazione)); intorno al lago a SE valore 1 (fitta vegetazione)
+#per immagine 2017 osservo: valori NDVI di -1 (rimanenza lago a SE); il resto del lago riporta valori tra 0.5 e 1 (aumenta superfice vegetativa)
 
 
-#DIFFERENZA di RIFLETTANZA TRA LAKE CHAD 2017 (chad3) E LAKE CHAD 1973 (chad1)
+#TIME SERIES ANALYSIS: LAKE CHAD 2017 (chad3) E LAKE CHAD 1973 (chad1)
 
-setwd("C:/lab/CH")
-library(raster)
-
+#utilizzo la funzione raster per richiamare solo la prima banda
 chad1 <- raster("chad1973.jpg")
-chad1 #info
+chad1 
 
-cl <- colorRampPalette(c("dark blue","light blue","light green","orange", "yellow")) (200)
+#imposto una colorramppalette a scelta
+cl <- colorRampPalette(c("dark blue","light blue","light green","orange","yellow")) (200)
 plot(chad1, col=cl)
 
 chad3 <- raster("chad2017.jpg")
-chad3 #info
+chad3 
 plot(chad3, col=cl)
 
-par(mfrow=c(1,2))
-plot(chad1, col=cl)
-plot(chad3, col=cl)
-
-# faccio la differenza tra la mappa del 2017 (chad3) e quella del 1973 (chad1) e la plotto nella colorramppalette creata prima
+#faccio la differenza tra la mappa del 2017 (chad3) e quella del 1973 (chad1) e la plotto nella colorramppalette creata prima
 CHdif1 <- chad3 - chad1
 plot(CHdif1, col=cl)
 
-#inverto, facendo 1973 - 2017
+#inverto, facendo 1973 - 2017 (solo per fare una "prova del 9")
 CHdif2 <- chad1 - chad3
 plot(CHdif2, col=cl)
 
-#questo par mi serve solo per osservare bene le due differenze messe a confronto
+#(questo par mi serve solo per osservare le due differenze messe a confronto)
 #noto che nella prima (CHdif1) la differenza si concentra nell' arancio-giallo (differenza in positivo poiché chad2017 ha valori più alti)
 #nella seconda (CHdif2) la differenza si concentra nel colore azzurro (differenza in negativo che conferma che chad1973 ha valori inferiori)
 par(mfrow=c(1,2))
 plot(CHdif1, col=cl)
 plot(CHdif2, col=cl)
 
-#plotto tutte le immagini insieme per osservare:
+#plotto tutte le immagini insieme per osservare la time series analysis:
 #Chad Lake nel 1973, Chad Lake nel 2017 e differenza tra Chad Lake 2017 e Chad Lake 1973
 par(mfrow=c(1,3))
 plot(chad1, col=cl, main="Chad Lake in 1973")
 plot(chad3, col=cl, main="Chad Lake in 2017")
 plot(CHdif1, col=cl, main="Difference (2017 - 1973)")
 
-
 #SPECTRAL SIGNATURES
-library(raster)
-setwd("C:/lab/CH")
 
-#gdal è la libreria generale per dati geospaziali sia raster sia vettoriali
-library(rgdal) #(posso anche non usarla)
 chad1 <- brick("chad1973.jpg")
 plotRGB(chad1, r=1, g=2, b=3, stretch="hist")
 
@@ -131,18 +121,18 @@ click(chad1, id=T, xy=T, cell=T, type="p", pch=16, col="yellow")
 #  x     y   cell        chad1973.1 chad1973.2 chad1973.3
 # 1 354.5 124.5 307987        6         71         73
 
-#PUNTO 1 N (interessante messo a confronto con il terzo)
-#riflettanza banda 1 = 1 (IR) 
+#PUNTO 1 N 
+#riflettanza banda 1 = 1 (NIR) 
 #riflettanza banda 2 = 53 (red) 
 #riflettanza banda 3 = 67 (green)
 
-#PUNTO 2 Centro-W (interessante messo a confronto con il terzo, ma volendo con entrambi)
-#riflettanza banda 1 = 7 (IR)
+#PUNTO 2 Centro-W 
+#riflettanza banda 1 = 7 (NIR)
 #riflettanza banda 2 = 45 (red) 
 #riflettanza banda 3 = 58 (green)
 
-#PUNTO 3 S (interessante messo a confronto con tutti e tre)
-#riflettanza banda 1 = 6 (IR)
+#PUNTO 3 S 
+#riflettanza banda 1 = 6 (NIR)
 #riflettanza banda 2 = 71 (red) 
 #riflettanza banda 3 = 73 (green)
 
@@ -157,74 +147,24 @@ click(chad2, id=T, xy=T, cell=T, type="p", pch=16, col="yellow")
 #  x     y   cell      chad1987.1 chad1987.2 chad1987.3
 # 1 231.5 219.5 241744      25          4          3
 #  x     y   cell      chad1987.1 chad1987.2 chad1987.3library(raster)
-setwd("C:/lab/CH")
+#1 353.5 123.5 308682       165        31         20
 
-#gdal è la libreria generale per dati geospaziali sia raster sia vettoriali
-library(rgdal) #(posso anche non usarla)
-chad1 <- brick("chad1973.jpg")
-plotRGB(chad1, r=1, g=2, b=3, stretch="hist")
-
-#utilizzo la funzione CLICK per cliccare su una mappa e ottenere informazioni relative a quel punto
-#in questo caso, le info sono relative alla riflettanza
-#T= true
-#id= argomento che stabilisce se voglio creare un identificativo per ogni punto
-#xy= arg che indica che vogliamo utilizzare un'informazione parziale
-#cell= arg che indica che clicco su un pixel
-#type= arg che indica il tipo di click, in questo caso il "punto, p"
-#pch= arg che indica lo stile del punto
-
-click(chad1, id=T, xy=T, cell=T, type="p", pch=16, col="yellow")
-#OUTPUT:
-#  x     y   cell        chad1973.1 chad1973.2 chad1973.3
-# 1 201.5 335.5 160978        1         53         67
-#  x     y   cell        chad1973.1 chad1973.2 chad1973.3
-# 1 232.5 218.5 242441        7         45         58
-#  x     y   cell        chad1973.1 chad1973.2 chad1973.3
-# 1 354.5 124.5 307987        6         71         73
-
-#PUNTO 1 N (interessante messo a confronto con il terzo)
-#riflettanza banda 1 = 1 (IR) 
-#riflettanza banda 2 = 53 (red) 
-#riflettanza banda 3 = 67 (green)
-
-#PUNTO 2 Centro-W (interessante messo a confronto con il terzo, ma volendo con entrambi)
-#riflettanza banda 1 = 7 (IR)
-#riflettanza banda 2 = 45 (red) 
-#riflettanza banda 3 = 58 (green)
-
-#PUNTO 3 S (interessante messo a confronto con tutti e tre)
-#riflettanza banda 1 = 6 (IR)
-#riflettanza banda 2 = 71 (red) 
-#riflettanza banda 3 = 73 (green)
-
-
-chad2 <- brick("chad1987.jpg")
-plotRGB(chad2, r=1, g=2, b=3, stretch="hist")
-
-click(chad2, id=T, xy=T, cell=T, type="p", pch=16, col="yellow")
-#OUTPUT:
-#  x     y   cell      chad1987.1 chad1987.2 chad1987.3
-# 1 201.5 336.5 160282      138        103         83
-#  x     y   cell      chad1987.1 chad1987.2 chad1987.3
-# 1 231.5 219.5 241744      25          4          3
-# 1 353.5 123.5 308682      165         31         20
-
-#PUNTO 1 N
-#riflettanza banda 1 = 138 (IR) (rispetto a chad1 aumenta di molto: inizia a formarsi copertura vegetale)
+#PUNTO 1 N 
+#riflettanza banda 1 = 138 (NIR) 
 #riflettanza banda 2 = 103 (red) 
 #riflettanza banda 3 = 83 (green)
 
-#PUNTO 2 Centro-W
-#riflettanza banda 1 = 25 (IR)
+#PUNTO 2 Centro-W 
+#riflettanza banda 1 = 25 (NIR)
 #riflettanza banda 2 = 4 (red) 
 #riflettanza banda 3 = 3 (green)
 
-#PUNTO 3 S
-#riflettanza banda 1 = 165 (IR) (niente acqua)
+#PUNTO 3 S 
+#riflettanza banda 1 = 165 (NIR)
 #riflettanza banda 2 = 31 (red) 
 #riflettanza banda 3 = 20 (green)
-    
 
+    
 chad3 <- brick("chad2017.jpg")
 plotRGB(chad3, r=1, g=2, b=3, stretch="hist")
 
@@ -238,17 +178,17 @@ click(chad3, id=T, xy=T, cell=T, type="p", pch=16, col="yellow")
 # 1 354.5 124.5 307987     19         58         73
 
 #PUNTO 1 N
-#riflettanza banda 1 = 151 (IR)
+#riflettanza banda 1 = 151 (NIR)
 #riflettanza banda 2 = 22 (red) 
 #riflettanza banda 3 = 52 (green)
 
 #PUNTO 2 Centro-W
-#riflettanza banda 1 = 164 (IR)
+#riflettanza banda 1 = 164 (NIR)
 #riflettanza banda 2 = 29 (red) 
 #riflettanza banda 3 = 59 (green)
 
 #PUNTO 3 S
-#riflettanza banda 1 = 19 (IR) (ritorna acqua)
+#riflettanza banda 1 = 19 (NIR) (ritorna acqua)
 #riflettanza banda 2 = 58 (red) 
 #riflettanza banda 3 = 73 (green)
 
@@ -333,13 +273,13 @@ labs(x="band", y="reflactance Lake Chad 1973-2017")
 #voglio osservare il ritorno dell'acqua in quel punto 
 
 band <- c(1,2,3)
-P3a <- c(6,71,73) #presenza di acqua (valore basso nella banda IR)
-P3b <- c(165,31,28) #assenza di acqua, presenza di vegetazione (alto valore nella banda IR)
-P3c <- c(19,58,73) #ritorno dell'acqua (valore basso nella banda IR)
+P3a <- c(6,71,73) #presenza di acqua (valore basso nella banda NIR)
+P3b <- c(165,31,28) #assenza di acqua, presenza di vegetazione (alto valore nella banda NIR)
+P3c <- c(19,58,73) #ritorno dell'acqua (valore basso nella banda NIR)
 
 data.frame(band,P3a, P3b, P3c)
-spectralsw <- data.frame(band,P3a, P3b, P3c)
-
+spectralsw <- data.frame(band, P3a, P3b, P3c)
+#tramite l'argomento size scelgo lo spessore della linea
 ggplot(spectralsw, aes(x=band))+ 
 geom_line (aes(y=P3a), color="red", size=1)+
 geom_line (aes(y=P3b), color="green",size=1)+
@@ -347,15 +287,16 @@ geom_line (aes(y=P3c), color="blue", size=1)+ #noto che assume simile andamento 
 labs(x="band", y="reflactance") 
 
 
-#Correlation between Lake Chad water level and Sahel rainfall anomalies
-install.packages("readxl")
+#CORRELATION BETWEEN LAKE CHAD WATER LEVEL AND SAHEL RAINFALL ANOMALIES
+install.packages("readxl")#installo il pacchetto readxl per importare file excel
 setwd("C:/lab/CH")
 library(readxl)
 
+#LAKE CHAD WATER LEVEL
 #ho ricavato i dati delle fluttuazione del livello del lago dal seguente sito: http://hydroweb.theia-land.fr/hydroweb/view/L_tchad?lang=en
 #ho riportato in un documento excel gli anni dal 1960 al 2020 (di 5 anni in 5 anni) in una colonna e i valori del livello del lago in un'altra
 #per i valori, ho scelto i mesi da giugno ad ottobre e ne ho fatto la media
-#ho utilizzato la libreria read_excel per importare il file creato
+#ho utilizzato la funzione read_excel per importare il file creato
 chadmslm <- read_excel("levelchad.xlsx")                                                                             
 chadmslm
 # A tibble: 13 x 2
@@ -375,18 +316,20 @@ chadmslm
 #12  2015 280.0                             
 #13  2020 280.4    
 
+#creo il dataframe con i dati importati da excel
 lakelevel<- data.frame(chadmslm)
 colnames(lakelevel)[2] <- "Lake_Chad_water_level"
+
 #plotto il grafico delle fluttuazioni del livello del lago
 plot(lakelevel$year, lakelevel$Lake_Chad_water_level, xlab="year", ylab="a.m.s.l.", col="red", type="o", cex=3, main="Lake-level fluctuations")
 
 
-
+#SAHEL RAINFALL ANOMALY
 #"The Sahel rainy season is centered on June through October, and the means are taken for those months."
-#Ho ricavato i dati delle anomalie di precipitazione nel Sahel in questo sito: http://research.jisao.washington.edu/data/sahel/#values
+#Ho ricavato i dati delle anomalie di precipitazione nel Sahel da questo sito: http://research.jisao.washington.edu/data/sahel/#values
 #ho riportato in un documento excel gli anni dal 1960 al 2017 in una colonna e i valori sulle anomalie in un'altra
 #per i valori, ho scelto i mesi da giugno ad ottobre, ne ho fatto la media e ho moltiplicato il valore ottenuto x 0,1, così da avere la conversione in mm
-#ho utilizzato la libreria read_excel per importare il file creato
+#ho utilizzato la funzione read_excel per importare il file creato
 sahelprec <- read_excel("Sahel prec anomaly.xlsx")
 sahelprec
 #A tibble: 58 x 2
@@ -404,6 +347,7 @@ sahelprec
 # 10  1969                         22.0 
 # ... with 48 more rows
 
+#creo il dataframe con i dati importati da excel
 anomaly <- data.frame(sahelprec)
 anomaly
 #  YEAR     SAHEL.RAINFALL.ANOMALY..mm.
@@ -422,5 +366,8 @@ anomaly
 #plotto il grafico delle anomalie di precipitazione
 plot(anomaly$YEAR, anomaly$SAHEL.RAINFALL.ANOMALY..mm., xlab="year", ylab="mm", col="blue", type="o", cex=3, main="Sahel rainfall anomaly")
 
-
+#RESULTS
+#si osservauna effettiva correlazione tra i due grafici: da metà anni '60=>inizia a diminuire il livello del lago e allo stesso tempo un netto calo delle precipitazioni (circa -8mm)
+#da metà degli anni '80 forte diminuzione delle piogge e conseguentemente una netta riduzione del livello del lago
+#in seguito agli anni 2000, il livello del lago ha ricominciato a salire, ma non è più ritornato ai valori originari
 
